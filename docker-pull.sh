e495e0101216231cd668f5bf2e80ed95c9da85f2
piragenth-ltfb <linuxtutorialforbeginners@gmail.com>
2022-08-22 19:39:12 -0500

    piragenth

---
+++ docker-pull.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker pull ubuntu
+docker pull debian
+docker pull linuxserver/code-server
+docker pull linuxserver/webtop:ubuntu-xfce
+docker pull linuxserver/webtop:ubuntu-kde
+docker pull linuxserver/webtop:arch-kde
+docker pull linuxserver/webtop:ubuntu-mate
+docker pull linuxserver/webtop:fedora-kde
+docker pull linuxserver/webtop:ubuntu-i3
+docker pull linuxserver/webtop:ubuntu-openbox
+docker pull linuxserver/webtop:ubuntu-icewm
