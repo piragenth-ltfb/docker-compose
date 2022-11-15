d8fdc736f9ec42789940bee7840c89ff434cb89f
piragenth-ltfb <linuxtutorialforbeginners@gmail.com>
2022-11-15 20:19:54 +0530

    docker pull image.py

---
+++ docker-pull-image.py
@@ -0,0 +1,21 @@
+import os
+
+docker_images = os.popen("docker images --format {{'.Repository'}}").read()
+image = ''
+list = []
+
+def formating_pulling(docker_images, image, list, counts=0):
+    print(os.system('docker images'))
+#Formating docker_images list and renaming
+    for superate in docker_images:
+        if superate != '\n':
+            image += superate
+        else:
+            list.append(image)
+            image = ''
+            counts+= 1
+    print(f'Total {counts} Images')
+    print(list)
+    for images in list:
+        print(os.system(f'docker pull {images}'))
+formating_pulling(docker_images,image,list)
